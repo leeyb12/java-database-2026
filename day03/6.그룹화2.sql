ac19a7ab23ffdc8a1d0e99b73ddfb17f7ece6732
Yunbeom <dbsqja1233@naver.com>
2026-03-04 08:59:11 +0900

    lyb

--- day03/6.그룹화2.sql
+++ day03/6.그룹화2.sql
@@ -1,9 +1,9 @@
 -- scott 계정!
--- 부서번호와 직군별 그룹화
+-- 부서번호와 직군별 그룹화 
 SELECT deptno, job
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
+     , count(*)
+     , max(sal)
+     , avg(sal)
   FROM emp
  GROUP BY deptno, job
  ORDER BY deptno;
@@ -11,96 +11,96 @@ SELECT deptno, job
 
 -- ROLLUP 함수 적용한 그룹화
 SELECT deptno
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
+     , count(*)
+     , max(sal)
+     , avg(sal)
   FROM emp
  GROUP BY deptno
  ORDER BY deptno;
 
 SELECT deptno
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
-  FROM emp 
+     , count(*)
+     , max(sal)
+     , avg(sal)
+  FROM emp
  GROUP BY ROLLUP(deptno)
  ORDER BY deptno;
 
 -- ROLLUP 첫번째 컬럼의 값으로 소계, 전체 총계를 도출
 SELECT deptno, job
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
+     , count(*)
+     , max(sal)
+     , avg(sal)
+     , GROUPING(job)
+     , GROUPING(deptno)
+     , GROUPING_ID(deptno, job)
   FROM emp
  GROUP BY ROLLUP(deptno, job)
- ORDER BY deptno;
+ ORDER BY deptno, job;
 
 SELECT deptno, job
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
+     , count(*)
+     , max(sal)
+     , avg(sal)
   FROM emp
  GROUP BY ROLLUP(deptno), job
  ORDER BY deptno;
 
 SELECT deptno, job
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
+     , count(*)
+     , max(sal)
+     , avg(sal)
+  FROM emp
+ GROUP BY deptno, ROLLUP(job)
+ ORDER BY deptno;
+
+SELECT deptno, job
+     , count(*)
+     , max(sal)
+     , avg(sal)
   FROM emp
  GROUP BY job, ROLLUP(deptno)
  ORDER BY job;
-ALTER 
-
-
-
-
-
-
-
 
 
 
 -- 각각의 컬럼별로 소계, 전체 총계를 도출
 SELECT deptno, job
-	 , count(*)
-	 , max(sal)
-	 , avg(sal)
-	 , GROUPING(job)
-	 , GROUPING(deptno)
+     , count(*)
+     , max(sal)
+     , avg(sal)
   FROM emp
  GROUP BY CUBE(deptno, job)
  ORDER BY deptno;
 
--- GROUPIG SETS - 차후
+-- GROUPING SETS - 차후
 -- GROUPING 함수 : SELECT절에 GROUPING 적용
 -- 실무에서는 각 그룹(다중행)함수에 별명을 지정사용
 SELECT deptno, job
-	 , count(*) AS "직원수"
-	 , max(sal) AS "최고급여"
-	 , avg(sal) AS "최소급여"
-	 , GROUPING(job) AS "JOB_GRID"
-	 , GROUPING(deptno) AS "DEP_GRID"
-	 , GROUPING_ID(deptno, job) AS "DEP_JOB_GRID"
-  FROM EMP
+     , count(*) AS "직원수"
+     , max(sal) AS "최고급여"
+     , avg(sal) AS "최소급여"
+     , GROUPING(job) AS "JOB_GRID"
+     , GROUPING(deptno) AS "DEP_GRID"
+     , GROUPING_ID(deptno, job) AS "DEP_JOB_GRID"
+  FROM emp
  GROUP BY CUBE(deptno, job)
  ORDER BY deptno;
 
-
 -- ROLLUP, CUBE를 몰라도 SubQuery로 구현 가능
 
--- PIVOT
--- 부서별, 직책별로 그룹화 최고 급여 조회
-SELECT dept_no, job, MAX(sal)
+-- PIVOT 
+-- 부서별, 직책별로 그룹화 최고 급여 조회, 피벗없이 일반적
+SELECT deptno, job, MAX(sal)
   FROM emp
  GROUP BY deptno, job
  ORDER BY deptno, job;
 
--- PIVOT 함수 사용
+-- PIVOT 함수 사용	
 SELECT *
   FROM (SELECT deptno, job, sal
-  	  FROM emp) -- subquery
+		  FROM emp) 
  PIVOT (MAX(SAL) FOR deptno IN (10, 20, 30))
-   ORDER BY job;
+  ORDER BY job;
 
 -- PIVOT 몰라도 CASE - WHEN으로 구현 가능
--- day03/7.조인.sql
+++ day03/7.조인.sql
@@ -1,2 +1,43 @@
 /* JOIN!!!!! */
 -- 관계형(Relational)DB, RDB에서 테이블을 합쳐서 표현하는 방법
+SELECT *
+  FROM emp;
+
+SELECT *
+  FROM dept;
+
+
+-- 합치기(그냥 합치기 - 카테시안곱)
+SELECT *
+  FROM emp, dept;
+
+-- 등가조인 inner-join, equi-join, simple join
+SELECT *
+  FROM emp, dept 
+ WHERE emp.deptno = dept.deptno;
+
+
+-- 이름 사용
+SELECT emp.empno, emp.ename, emp.job
+	 , emp.mgr, emp.hiredate, emp.sal
+	 , emp.sal, dept.deptno, dept.dname 
+  FROM emp, dept
+ WHERE emp.deptno = dept.deptno;
+
+-- 별명 사용
+SELECT e.empno, e.ename, e.job
+	 , e.mgr, e.hiredate, e.sal
+	 , e.sal, d.deptno, d.dname 
+  FROM emp e, dept d 
+ WHERE e.deptno = d.deptno;
+
+-- 비등가조인
+SELECT * 
+  FROM emp;
+
+SELECT *
+  FROM salgrade;
+
+SELECT *
+  FROM emp e, salgrade s
+ WHERE e.sal BETWEEN s.losal AND s.hisal;
